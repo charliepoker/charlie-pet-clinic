70e34cd0e3c269bddac0f5b3aeb0b2e189a1897a
charliepoker <iheanachocharlie@gmail.com>
2022-06-19 06:16:54 +0100

    initial commit

---
+++ src/main/java/com/charlie/marleypetclinic/MarleyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.charlie.marleypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarleyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarleyPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charlie/marleypetclinic/MarleyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie.marleypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarleyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
