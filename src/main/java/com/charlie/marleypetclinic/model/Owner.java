af9e72f707730f0f152c0b467c9508ec599904d3
charliepoker <iheanachocharlie@gmail.com>
2022-06-22 05:28:16 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/charlie/marleypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.charlie.marleypetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/charlie/marleypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.charlie.marleypetclinic.model;
+
+public class Vet extends Person{
+}
