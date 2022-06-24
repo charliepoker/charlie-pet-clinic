c4384a1450c703f0bbd17232c156c9fcf35036ac
charliepoker <iheanachocharlie@gmail.com>
2022-06-24 14:58:23 +0100

    refactoring to multimodule maven project.

--- marley-pet-clinic-web/src/test/java/com/charlie/marleypetclinic/MarleyPetClinicApplicationTests.java
+++ marley-pet-clinic-web/src/test/java/com/charlie/marleypetclinic/MarleyPetClinicApplicationTests.java
@@ -1,8 +1,11 @@
-package marleypetclinic;
+package com.charlie.marleypetclinic;
 
 import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class MarleyPetClinicApplicationTests {
 
@@ -11,3 +14,4 @@ class MarleyPetClinicApplicationTests {
     }
 
 }
+
